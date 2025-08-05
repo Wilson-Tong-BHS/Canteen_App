dcafa807e0b38517d998dc41c12a942b7f0ed00f
Wilson Tong <205688553+Wilson-Tong-BHS@users.noreply.github.com>
2025-08-05 20:47:18 +1200

    Update, Added photos, and database

--- routes.py
+++ routes.py
@@ -1,11 +1,37 @@
 from flask import Flask, render_template
+from datetime import datetime
+import sqlite3
 
+#Creates Flask Application
 app = Flask(__name__)
 
+def get_db():
+    db= sqlite3.connect('food.db')
+    db.row_factory = sqlite3.Row
+
+    return db
 
 @app.route('/')
 def home():
-    return render_template('home.html', title='HOME')
+    conn = sqlite3.connect('food.db')
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM menu_items')
+    food = cur.fetchall()
+    conn.close()
+    return render_template('home.html', title='HOME', food = food)
+
+
+
+
+#Route to show all foods avaiable in hot food.
+@app.route('/hot_food')
+def all_hot_food():
+    conn = sqlite3.connect('food.db')
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM menu_items WHERE category="Hot Food (every day)"')
+    hot_food = cur.fetchall()
+    conn.close()
+    return render_template('hot_food.html', title='HOME', hot_food = hot_food)
 
 if __name__ == '__main__':
     app.run(debug=True)
